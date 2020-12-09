d4164c46115e45cb16f835134c6e55b961ee4ba7
mabroukST-QA <st.belaid.mabrouk@gmail.com>
2020-12-09 11:15:32 +0100

    add new file for test

---
+++ Test script/pycharm_launcher.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+
+def main():
+    print("hiii")
+    os.system("//home//tunvqtvalidadm//Downloads//pycharm-2020.1//bin//pycharm.sh")
+
+
+
+
+
+if __name__=='__main__' :
+    main()
